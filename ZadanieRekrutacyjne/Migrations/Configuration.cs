a5dad762b58f812f275124567e6891d6c4ca3e85
Jakub Zimny <jakubzimny@onet.pl>
2021-03-12 08:33:59 +0100

    Dodaj pliki projektów.

---
+++ ZadanieRekrutacyjne/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ZadanieRekrutacyjne.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ZadanieRekrutacyjne.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ZadanieRekrutacyjne.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ ZadanieRekrutacyjne/Models/Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZadanieRekrutacyjne.Models
+{
+    public class Tree
+    {
+        public int TreeId { get; set; }
+
+        public string Name { get; set; }
+
+        public int ParentID { get; set; }
+
+        public virtual ICollection<Tree> Kids { get; set; }
+
+    }
+}
